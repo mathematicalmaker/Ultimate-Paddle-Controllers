128cefe7fe3d567d4ce5110e996b66377015b6fb
Jason Hoomani <36106281+jasonh531@users.noreply.github.com>
2025-03-02 12:28:01 -0600

    Cleaned up testing code from custom_name.py

--- custom_name.py
+++ custom_name.py
@@ -1,16 +1,8 @@
-#import pprint
 Import("env")
 
 board_config = env.BoardConfig()
-board_config.update("build.usb_product", "Paddles Set 1")
-board_config.update("name", "Rotary Encoder Paddles")
-board_config.update("vendor", "mathematicalmaker")   
-board_config.update("build.usb_description", "Test Description")
-# 
-'''
-print("Board Configuration:")
-pprint.pprint(board_config.__dict__)
-
-print("\nEnvironment Dump:")
-pprint.pprint(env.Dictionary())
-'''
+board_config.update("build.usb_product", "Set1 - Paddles") #USB Product name
+board_config.update("build.hwids", [
+  ["0x1209", "0x0001"], #Generic VID/PID
+  ["0x2886", "0x002F"]  #Orignal values for Seeeduino XIAO 2nd pair
+])
